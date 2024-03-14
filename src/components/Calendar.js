4e685d87271e05b750b27774b7f99d7c5b2ff850
VIKRANT SINGH <VIKRANTSINGH.VS53@GMAIL.COM>
2024-03-15 01:00:54 +0530

    updated css

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -7,8 +7,8 @@ import dayGridPlugin from '@fullcalendar/daygrid'; // Using the dayGrid plugin
 const Calendar = () => {
   const events = [
     // Add your calendar events here
-    { title: 'Flight to Delhi', date: '2024-03-14' },
-    { title: 'Holi Hae Bhailog', date: '2024-03-25' },
+    { title: 'Expected Delivery for A', date: '2024-03-14' },
+    { title: 'Expected Delivery for B', date: '2024-03-25' },
     // Add more events as needed
   ];
 
